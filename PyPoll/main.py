93b00e47d9ad631c2f11a994cf3c463cb68aace5
freemansuruma <surumafreeman@gmail.com>
2017-11-11 16:25:32 -0500

    Completed code to analyze election data

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,11 +9,38 @@ with open(file_path, newline='') as csvfile:
 
     file_path_pd = pd.read_csv(file_path)
 
+# Find the total votes cast
     total_votes_cast = file_path_pd['Voter ID'].count()
 
+# Find the number of candidates
     candidates = file_path_pd['Candidate'].unique()
 
+# Find the number of votes for each candidate
     total_votes_per_candidate = file_path_pd['Candidate'].value_counts()
+# Sort the list so that the candidate with the most votes is the first item    
+    total_votes_per_candidate.sort_values(ascending=False)
 
-    pct_votes_per_candidate = total_votes_per_candidate/total_votes_cast
+# Find the percentage of votes for each candidate
+    pct_votes_per_candidate = (total_votes_per_candidate/total_votes_cast)*100
+
+#Convert the series to lists
+    candidates_list = candidates.tolist()
+
+    votes_list = total_votes_per_candidate.tolist()
+
+    pct_list = pct_votes_per_candidate.tolist()
+
+# Display the information required
+
+    print('Election Results')
+    print('-----------------------------------------')
+    print('Total Votes: ' + str(total_votes_cast))
+    print('-----------------------------------------')
+    i = 0
+    for item in candidates_list:
     
+        print(item + ': ' + str(round(pct_list[i])) + '% ' + '(' + str(votes_list[i]) + ')')
+        i=+1
+
+    print('-----------------------------------------')
+    print('Winner: ' + str(candidates_list[0]))
